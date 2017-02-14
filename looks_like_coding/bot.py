ecf3bb9940c20bb4a72e781e8c9ea903b6503ed6
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2017-02-14 13:59:44 +0900

    quick fix failed to login

--- looks_like_coding/bot.py
+++ looks_like_coding/bot.py
@@ -67,23 +67,27 @@ import getpass
 class GreetBot(fbchat.Client):
     """Saying hi. Copied from EchoBot."""
     
-    def __init__(self, email, password):
+    def __init__(self, email, password, name="GreetBot", master="Hoang"):
         """Create the bot with @param email and @param password."""
-        super().__init__(self, email, password)
-        self.name = "GreetBot"
+        fbchat.Client.__init__(self, email, password)
+        self.name = name
+        self.master = master
 
     def on_message(self, mid, author_id, author_name, message, metadata):
         """Call upon a message is received."""
         self.markAsDelivered(author_id, mid)
         self.markAsRead(author_id)
         print("{} said: {}".format(author_id, message))
-        self.send(author_id, "Hi, this is Hoang's bot, not Hoang.\
-                              My name is {}. Chat with me might be\
-                              more interesting...".format(self.name))
-        self.send(author_id, "I cannot do much for now...")
+        if (author_id == self.id):
+            pass
+        else:
+            self.send(author_id, "Hi, this is {}'s bot, not Hoang.\
+                                  My name is {}. Chat with me might be\
+                                  more interesting...".format(self.master, self.name))
+            self.send(author_id, "I cannot do much for now...")
 
 
-class Dwight(fbchat.Client):
+class Dwight(GreetBot):
     """Hoang's robot butler, Dwight.""" 
     pass
 
@@ -92,7 +96,7 @@ def test():
     """Test the greet bot.""" 
     email = input("Enter your email:" )
     password = getpass.getpass("Type in your password: ")
-    bot = GreetBot("hoangnt@me.com", "7844236Ok")
+    bot = GreetBot(email, password)
     bot.listen()
 
 
