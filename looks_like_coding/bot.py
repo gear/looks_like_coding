bf14639dd852d2eded62f4029752191d46f57e64
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2017-02-14 13:00:06 +0900

    greeting bot

---
+++ looks_like_coding/bot.py
@@ -0,0 +1,100 @@
+"""                                `/+o/.
+                       .+sso+/:oydyo/:-:+shdys/    `-:.     `-/+o+/`
+                   `/sdh+/::/::ss:`ymdhyso//hmMNyhNNms+ososys+/-:/shms/`
+                .+hNNy++oo+/.`.--/osyhdmNNMMMMMMMMMNdsssssoso+hhhhsoo+ymdo.
+              -smNy/+ymmmmmNNNNMNMMMMMNNNmmNMMMMMMMMMho:///:--shydNMMNdo-sNs`
+            -hNd+-sNMNdmNMMMNNNMNNNMMMddNMMNNmNMMMMMMNmy+///::/:-:/++ymNNdmMN:
+          `sNMs`+NMNNNMMMMNNNMMMMMMNmhyso///+ohMmoNMmoo+/::/-:oymNNmsosshdhmMM/
+         +NMMy`hMMMhyNMNMMNNNMds:-.`-:syddmNMMmyo`+yMMho:..-+//++omMNNNNNNNmdNMs
+       :mMMMh`yMNdodNNNMNMMMs.+sdmmmmmdhNMMMNhy/..`-syhNmdyssso+/.`:yNMMMMNMNMMMy
+      :NMNh:-+MMh+mdNNNNNMd.+NNMMMMMMMMmho:-......:--::ohNMMMMMMNmNy/.oNMNmNMNMMMs
+     :NMm+/dmmMNydyhNdhMMN.yMMNmhysso+:-``        ```.--:/+sdMMMMMNNNm:-mMNNNNMMMMy
+    :NMy/hNMMMMmNddsh/NmMy-Mms:..`.--.`                ``..-.:yNMMMMNMNs:NMMMNNNNMMy
+   :NNy/mMMMMMMmNMMshsNdMo/d-...``                       ```...-yMMMNNMd`NMMNMdmoNMM-
+  /mMm+NMNNMMNMNNNNNNNNMMmom/                              ```..`+NMMMMh`NMMMMNNdhNMh
+ +NMMmmMNyNMNMMMMMNmmmNMdNNyh+.                             ``````/NMMM::MMMMNMNNmNMN
++MNNMMMNymMNNMMMNNNNNMh+:+dNmddhyoo+`                        ````.`sMMN`sMNNMNNMNNNNN
+dNNNMNNddMNNNNNNmymMN+---::/shdhyyy:                         `````..hMo.NMNMNMMMNmMMd
+dNNNMMNmNNNmmNMNdNMM+.-..----.-:::.                          ````...:mh/NMMMNMMMNNMMh
+sMNNMMNMNNmyNMNdmNMo--.....                                  ``...---:dNMNMMNMMNNNMMN.
+:NNNMMMNNNsmMNmMNMy...`.-.`                                    `.-----:odNmmNMMMMMNMMo
+.NMMMmMMMNmMNNNNMm:-.```..                                       ``-----:/o//dMMMNMMMm
+.NMMMNMMNMMNMNNNNs--.``...                                          `....---..dMNMMMMM`
+.NNMMNNNNNMMMNNNN:-...`...                                           ```.....`+MMMMMMM.
+.MNNNNNNNMMMMMNNy.......-.`                                          ``..---.`.NMMMMMM`
+`NMNMMNNNMMNMMMm-...`.-----.`                                        ``.-----.`yMMMMMd
+ dMMMNNNNMMNNMMo`-....----..`          ``                      `.`` ```.------`:MMMMM-
+ /MMNMNNNMMNMMN-`.`..-.--.` `--..-:-.-.``..``               ```.-......--.----..NMMMd
+ `mMNMNNMMMNNMN.``...-.-../hddyysyhysyyso+--/::-..--...----:::+syyyyhhdddy+:-.-.hMMM:
+  :NNNNNNMMMMMN.`....--.:dy/:-.-/+++ososss+/:+shyo/::/:+os+:+syosyoso+/://ss//.`+MMm
+   +MdmNNMNMMMN+.--....:+-.-:+ooymdddmdhyo++ss+/yMo.`..oNsyhdhmdmmmmNmdo:-.--:+-:MM/
+  `y/..-+dNNMMMo-shhyo++--+sso-`dsymoso.smyso+//.od+/:/ho+yyhd/ymsNhyy./yy/``.-hhmm`
+  .s+md+- oMMMm``.-/sy//-.+/s.  odys+s-  /shyso+.sm+:::yd/hh+:`.hyyhy- `/y/.` `hs/s`
+  -oyMNyhs:NMMo     `.-`         .---` ``.`/::+s/ms````-mo+:`````.--` ````     `sNm`
+  `hsMh`.hymMM:       `-         `          .:+:hy`     od:-`                  .+sM-``
+   o+o/``-/mMM-        .-                ``.```hy`       s.`.`                 -/+M+``
+   .s `./NMMMM-         --            ````  `:ho`        .s`  ```             ./.+My`
+    /: `+MMdMM/          -.  `       `   ..+++-           :d/.             ``:o-`oMy
+     o. .sdNMMm`            `--:://+//.`-///:.           `.ohooo:-.`` `.-:+//:..`hMy
+     `s```.yMMMs                  ```     .y+  `::.:----.-``o:-::/:::--:::-----..mMo
+      :s` `oMNMN-                         :N+  -NNhy/:/sds./:..----------------`/MN.
+       +o``-NMNMd`                      `-syyoo++/.++:so/+yN+..--....-..-....--`dM+
+        +:.`oMNNN`                     .:-` `.::.` `--..---/+/---.```........-.:d:
+         ./++Ny::`                   `--`          .--..-----::-..```......---.s.
+           `:os.--`                  .`            `.. ``.------.`.```..-----.:o
+             `h-..`                 ``        .:syy/-/ydho-.--...`````.------.+.
+              +o`.`                        ./ymNNNNNNNmmNNNh:....``.```.-----:s
+              `h-`.                    -/+oyo/:----:---.--:+sso:........--::-+:
+               /d...                 `.++:  -:--/+:/oo+o++-.``--.....-----:-:y
+               `Md:.`                ``     `-:/+ooooo+/-........-----------yo
+                mNNs-`                     `..-/oo+://:/oo:......----------os
+                h:+md:.                  ...``.`         `------.---------++
+               `h..-+ddo.`                            ``.----------------s:
+                h` .--/ydy:`                   `...--------------------+y.
+                h`   ..--+yds+.`               `....----------------:+dN`
+               `y      `.-.-:sdhs:.`    `...````..----------------:smsdm
+               `h         .--..-+ymdy+/:----:----------------.-/shs+.`os
+               `h           `..--..:sdmmhyo/::----------::/+syhy/....`+-
+               -y              `..--..--/oosyyyhhhyyyssoooo/:.`...`.` /-
+               `.                  `..--.......................````   +`
+                                      `...------..-.........``
+                                          ``..-.--........``
+                                             ```.."""
+import fbchat
+import utils
+import getpass
+
+class GreetBot(fbchat.Client):
+    """Saying hi. Copied from EchoBot."""
+    
+    def __init__(self, email, password):
+        """Create the bot with @param email and @param password."""
+        super().__init__(self, email, password)
+        self.name = "GreetBot"
+
+    def on_message(self, mid, author_id, author_name, message, metadata):
+        """Call upon a message is received."""
+        self.markAsDelivered(author_id, mid)
+        self.markAsRead(author_id)
+        print("{} said: {}".format(author_id, message))
+        self.send(author_id, "Hi, this is Hoang's bot, not Hoang.\
+                              My name is {}. Chat with me might be\
+                              more interesting...".format(self.name))
+        self.send(author_id, "I cannot do much for now...")
+
+
+class Dwight(fbchat.Client):
+    """Hoang's robot butler, Dwight.""" 
+    pass
+
+
+def test():
+    """Test the greet bot.""" 
+    email = input("Enter your email:" )
+    password = getpass.getpass("Type in your password: ")
+    bot = GreetBot("hoangnt@me.com", "7844236Ok")
+    bot.listen()
+
+
+if __name__ == "__main__":
+    test()
